961caf3657e415ca03a321d3aec0dc42ba6a0fdd
Mark J. Lehman <markopolo@gmail.com>
2016-04-10 12:16:12 -0700

    Setup initial Toggl integration

---
+++ app/services/toggl/report.rb
@@ -0,0 +1,36 @@
+module Toggl
+  class Report
+    include HTTParty
+
+    attr_accessor :token, :workspace_id, :user_agent
+
+    base_uri 'https://toggl.com/reports/api/v2'
+
+    def initialize(token:, workspace_id:, user_agent:)
+      @token = token
+      @workspace_id = workspace_id
+      @user_agent = user_agent
+    end
+
+    def fetch(since: Time.now.monday)
+      self.class.get('/details', basic_auth: auth, query: query(since))
+    end
+
+    private
+
+    def auth
+      {
+        username: token,
+        password: "api_token"
+      }
+    end
+
+    def query(since)
+      {
+        workspace_id: workspace_id,
+        user_agent: user_agent,
+        since: since
+      }
+    end
+  end
+end
