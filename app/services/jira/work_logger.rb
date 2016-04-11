cb489f8a6d60c0c1c4aeb95aebebb91ed9e886ae
Mark J. Lehman <markopolo@gmail.com>
2016-04-11 00:02:08 -0700

    Tweak work logger to get it almost working

--- app/services/jira/work_logger.rb
+++ app/services/jira/work_logger.rb
@@ -2,26 +2,63 @@ module JIRA
   class WorkLogger
     include HTTParty
 
-    attr_accessor :issue_key, :time_entries
+    attr_accessor :username, :password, :time_entries
 
     base_uri 'https://hranswerlink.atlassian.net/rest/api/2'
 
-    def initialize(issue_key, time_entries)
-      @issue_key = issue_key
+    def initialize(username:, password:, time_entries:)
+      @username = username
+      @password = password
       @time_entries = time_entries
     end
 
-    def log
+    def log_all
       time_entries.each do |entry|
-        self.class.post("/issue/#{issue_key}/worklog", payload(entry))
+        log(entry)
       end
     end
 
     private
 
-    def payload(entry)
+    def log(entry)
+binding.pry
+      issue_key = parse_issue_key(entry)
+      payload = build_payload(entry)
+      # @TODO fix this - still doesn't connect properly -- getting this error:
+      # {"errorMessages"=>
+      # ["Unexpected character ('c' (code 115)): expected a valid value (number, String, array, object, 'true', 'false' or 'null')\n at [Source: org.apache.catalina.connector.CoyoteInputStream@11d429d; line: 1, column: 2]"]}
+      # The 'c' is from 'comment' in the payload
+      self.class.post("/issue/#{issue_key}/worklog", basic_auth: auth, headers: headers, body: payload)
+    end
 
-      JIRA::PayloadBuilder.new(entry)
+    def auth
+      {
+        username: username,
+        password: password
+      }
+    end
+
+    def headers
+      { 'Content-Type' => 'application/json' }
+    end
+
+    def build_payload(entry)
+      JIRA::PayloadBuilder.new(
+        start: entry['start'],
+        duration_in_seconds: entry['dur'],
+        comment: comment(entry)
+      ).build
+    end
+
+    # @TODO figure out how to capture both of this in one .match call with one set of regex
+    def parse_issue_key(entry)
+      matches = entry['description'].match(/(\[(?<issue_key>[^\]]*)\])/)
+      matches['issue_key'] if matches.present?
+    end
+
+    def comment(entry)
+      matches = entry['description'].match(/(\{(?<comment>[^\}]*)\})/)
+      matches['comment'] if matches.present?
     end
   end
 end
